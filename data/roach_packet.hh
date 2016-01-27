0ba187d8902c6506fc619085494e42dfe0df1886
Noah Oblath <nsoblath@mit.edu>
2016-01-27 15:05:54 -0500

    Setup the roach_packet struct based on Andre's input

--- data/roach_packet.hh
+++ data/roach_packet.hh
@@ -8,21 +8,29 @@
 #ifndef DATA_ROACH_PACKET_HH_
 #define DATA_ROACH_PACKET_HH_
 
-#include "member_variables.hh"
-
 #include <cinttypes>
+
+#define PAYLOAD_SIZE 8192 // 1KB
 
 namespace psyllid
 {
 
-    class roach_packet
-    {
-        public:
-            roach_packet();
-            virtual ~roach_packet();
-
-            mv_referrable( int8_t, time_value );
-            mv_referrable( double, freq_value );
+    struct roach_packet {
+        // first 64bit word
+        uint32_t f_unix_time;
+        uint32_t f_pkt_in_batch:20;
+        uint32_t f_digital_id:6;
+        uint32_t f_if_id:6;
+        // second 64bit word
+        uint32_t f_user_data_1;
+        uint32_t f_user_data_0;
+        // third 64bit word
+        uint64_t f_reserved_0;
+        // fourth 64bit word
+        uint64_t f_reserved_1:63;
+        uint64_t f_freq_not_time:1;
+        // payload
+        char f_data[PAYLOAD_SIZE];
     };
 
 } /* namespace psyllid */
