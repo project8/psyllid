89ad2f094aa2b84121e925cc3675e2932de0fcb2
Noah Oblath <nsoblath@gmail.com>
2017-01-09 15:41:25 -0800

    Made digitizer parameters configurable in egg_writer

--- source/daq/egg_writer.hh
+++ source/daq/egg_writer.hh
@@ -18,6 +18,38 @@
 namespace psyllid
 {
 
+    /*!
+     @class egg_writer
+     @author N. S. Oblath
+
+     @brief A consumer to that writes triggered time ROACH packets to an egg file.
+
+     @details
+
+     Parameter setting is not thread-safe.  Executing is thread-safe.
+
+     Node type: "egg-writer"
+
+     Available configuration values:
+     - "file-size-limit-mb": uint -- Not used currently
+     - "device": node -- digitizer parameters
+       - "bit-depth": uint -- bit depth of each sample
+       - "data-type-size": uint -- number of bytes in each sample (or component of a sample for sample-size > 1)
+       - "sample-size": uint -- number of components in each sample (1 for real sampling; 2 for IQ sampling)
+       - "record-size": uint -- number of samples in each record
+       - "acq-rate": uint -- acquisition rate in MHz
+       - "v-offset": double -- voltage offset for ADC calibration
+       - "v-range": double -- voltage range for ADC calibration
+
+     ADC calibration: analog (V) = digital * gain + v-offset
+                      gain = v-range / # of digital levels
+
+     Input Stream:
+     - 0: time_data
+     - 1: trigger_flag
+
+     Output Streams: (none)
+    */
     class egg_writer :
             public midge::_consumer< egg_writer, typelist_2( time_data, trigger_flag ) >,
             public control_access
@@ -30,6 +62,14 @@ namespace psyllid
             mv_accessible( unsigned, file_size_limit_mb );
             mv_referrable( std::string, filename ); /// used if f_daq_control is not set
             mv_referrable( std::string, description ); /// used if f_daq_control is not set
+
+            mv_accessible( unsigned, bit_depth ); // # of bits
+            mv_accessible( unsigned, data_type_size ); // # of bytes
+            mv_accessible( unsigned, sample_size );  // # of components
+            mv_accessible( unsigned, record_size ); // # of samples
+            mv_accessible( unsigned, acq_rate ); // MHz
+            mv_accessible( double, v_offset ); // V
+            mv_accessible( double, v_range ); // V
 
         public:
             virtual void initialize();
