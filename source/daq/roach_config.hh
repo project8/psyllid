7402cbd662266cfcaa770ab16ba095b8646d6118
Noah Oblath <nsoblath@gmail.com>
2017-02-20 14:08:04 -0800

    Added declaration of fmask_trigger_1ch_fpa in roach_config.hh

--- source/daq/roach_config.hh
+++ source/daq/roach_config.hh
@@ -15,10 +15,13 @@ namespace psyllid
 
     DECLARE_PRESET( streaming_1ch );
 #ifdef __linux__
-	DECLARE_PRESET( streaming_1ch_fpa );
+    DECLARE_PRESET( streaming_1ch_fpa );
 #endif
 
-	DECLARE_PRESET( fmask_trigger_1ch);
+    DECLARE_PRESET( fmask_trigger_1ch );
+#ifdef __linux__
+    DECLARE_PRESET( fmask_trigger_1ch_fpa );
+#endif
 } /* namespace psyllid */
 
 #endif /* PSYLLID_ROACH_CONFIG_HH_ */
