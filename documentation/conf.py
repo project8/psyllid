9d98159b450c1fae2e96389bd55b6f247a8a1dae
Noah Oblath <nsoblath@gmail.com>
2018-01-20 17:15:37 -0800

    Removed breathe extension

--- documentation/conf.py
+++ documentation/conf.py
@@ -70,7 +70,7 @@ else:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['breathe']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
