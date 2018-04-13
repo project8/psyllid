fb7f77ae7af3b287f1aba8a47bb4fdfd2c67dc3b
Ben LaRoque <benjamin.laroque@gmail.com>
2018-04-12 22:23:40 -0700

    loop setup in batch execution

--- source/control/batch_executor.cc
+++ source/control/batch_executor.cc
@@ -5,9 +5,15 @@
  *     Author: laroque
  */
 
-
-#include "logger.hh"
+//psyllid includes
 #include "batch_executor.hh"
+
+//non-psyllid P8 includes
+#include "dripline_constants.hh"
+#include "logger.hh"
+#include "message.hh"
+
+using dripline::request_ptr_t;
 
 namespace psyllid
 {
@@ -28,11 +34,35 @@ namespace psyllid
     {
     }
 
+
+    /* considering yaml that looks like:
+    batch-actions:
+        - type: cmd
+          rks: start-run
+          payload:
+            - duration: 200
+            - filenames: '["/tmp/foo_t.yaml", "/tmp/foo_f.yaml"]'
+    */
     void batch_executor::execute()
     {
         if ( ! f_actions_node.is_array() )
         {
             LWARN( plog, "batch-actions configuration is not an array" );
+            for ( scarab::param_array::iterator action_it = f_actions_node.as_array().begin();
+                  action_it!=f_actions_node.as_array().end();
+                  ++action_it )
+            {
+                //access each action (still a node) as *action_it
+                // get this from the iterator
+                scarab::param_node* a_payload = new scarab::param_node();
+                // is there a map for strings to dripline::op_t? should dripline have one?
+                dripline::op_t a_msg_op = dripline::op_t::cmd;
+                request_ptr_t a_reqeust = dripline::msg_request::create(
+                                                  a_payload, //*action_it->as_node()->at( "payload" ),//payload
+                                                  a_msg_op, //1, //msg_op [op_t]
+                                                  "", //routing key [std::string]
+                                                  "" );//, //reply-to [std::string]
+            }
         }
     }
 
