4a0a986ae04a95a4dd5029ed287654b29ceb919b
Ben LaRoque <benjamin.laroque@gmail.com>
2018-07-10 21:42:44 -0700

    request receiver implements set_condition

--- source/control/request_receiver.hh
+++ source/control/request_receiver.hh
@@ -37,6 +37,7 @@ namespace psyllid
 
             void execute();
 
+            mv_accessible_noset( scarab::param_node, set_conditions );
         private:
             virtual void do_cancellation();
 
