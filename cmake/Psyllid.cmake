01e1fdf3cfc650ecf861fce6dbc62143693fb1d1
Ben LaRoque <benjamin.laroque@gmail.com>
2018-11-29 10:33:20 -0800

    cmake gymnastics

---
+++ cmake/Psyllid.cmake
@@ -0,0 +1,11 @@
+# Psyllid.cmake
+# Macros for building a project using Psyllid
+# Author: B. LaRoque (based on Nymph.cmake by N. Oblath)
+
+set (PSYLLID_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
+message( STATUS "%%%%%%%%%%%%%%%% psyllid_dir is ${PSYLLID_DIR}" )
+
+macro (psyllid_build_executables)
+    message( STATUS "psyllid_dir is ${PSYLLID_DIR}" )
+    add_subdirectory (${PSYLLID_DIR}/source/applications)
+endmacro ()
