8be0daab72ea0c52b3a589016b8014b99322fd52
Ben LaRoque <benjamin.laroque@gmail.com>
2018-11-29 12:59:35 -0800

    removing extra prints

--- cmake/Psyllid.cmake
+++ cmake/Psyllid.cmake
@@ -5,6 +5,5 @@
 set (PSYLLID_DIR ${CMAKE_CURRENT_LIST_DIR}/..)
 
 macro (psyllid_build_executables)
-    message( STATUS "%%%%%%% psyllid_dir is <${PSYLLID_DIR}>" )
     add_subdirectory (${PSYLLID_DIR}/source/applications)
 endmacro ()
