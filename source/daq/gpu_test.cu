accbb74232f0cefad5fd4b987ea41de2202940c0
Florian Thomas <flthomas@students.uni-mainz.de>
2020-11-17 18:06:43 +0100

    Modify GPU test node
    Remove some unused code and probably questionable assignment. The test
    node does not function as a proper psyllid node at the moment.

--- source/daq/gpu_test.cu
+++ source/daq/gpu_test.cu
@@ -39,8 +39,8 @@ namespace psyllid{
 
     void gpu_test::execute( midge::diptera* a_midge)
     {
-    	out_stream< 0 >() = in_stream< 0 >();
-    	out_stream< 1 >() = in_stream< 1 >();
+    	//out_stream< 0 >() = in_stream< 0 >();
+    	//out_stream< 1 >() = in_stream< 1 >();
     	LDEBUG( plog, "Executing gpu_test" );
     	hello_gpu<<<1, 4>>>();
     	cudaDeviceSynchronize();
@@ -66,45 +66,18 @@ namespace psyllid{
     void gpu_test_binding::do_apply_config( gpu_test* a_node, const scarab::param_node& a_config ) const
     {
         LDEBUG( plog, "Configuring gpu_test with:\n" << a_config );
-        /*a_node->set_time_length( a_config.get_value( "time-length", a_node->get_time_length() ) );
-        a_node->set_freq_length( a_config.get_value( "freq-length", a_node->get_freq_length() ) );
-        a_node->set_udp_buffer_size( a_config.get_value( "udp-buffer-size", a_node->get_udp_buffer_size() ) );
-        a_node->set_time_sync_tol( a_config.get_value( "time-sync-tol", a_node->get_time_sync_tol() ) );
-        a_node->set_start_paused( a_config.get_value( "start-paused", a_node->get_start_paused() ) );
-        a_node->set_force_time_first( a_config.get_value( "force-time-first", a_node->get_force_time_first() ) );*/
         return;
     }
 
     void gpu_test_binding::do_dump_config( const gpu_test* a_node, scarab::param_node& a_config ) const
     {
         LDEBUG( plog, "Dumping gpu_test configuration" );
-        /*a_config.add( "time-length", scarab::param_value( a_node->get_time_length() ) );
-        a_config.add( "freq-length", scarab::param_value( a_node->get_freq_length() ) );
-        a_config.add( "udp-buffer-size", scarab::param_value( a_node->get_udp_buffer_size() ) );
-        a_config.add( "time-sync-tol", scarab::param_value( a_node->get_time_sync_tol() ) );
-        a_config.add( "start-paused", scarab::param_value( a_node->get_start_paused() ) );
-        a_config.add( "force-time-first", scarab::param_value( a_node->get_force_time_first() ) );*/
         return;
 
     }
 
     bool gpu_test_binding::do_run_command( gpu_test* a_node, const std::string& a_cmd, const scarab::param_node& ) const
     {
-        /*if( a_cmd == "freq-only" )
-        {
-            a_node->switch_to_freq_only();
-            return true;
-        }
-        else if( a_cmd == "time-and-freq" )
-        {
-            a_node->switch_to_time_and_freq();
-            return true;
-        }
-        else
-        {
-            LWARN( plog, "Unrecognized command: <" << a_cmd << ">" );
-            return false;
-        }*/
 
     	LDEBUG( plog, "Do run command gpu_test" );
     	return true;
