acf365a8c64d779b31bb6e8bc5e60b4acad7600a
Noah Oblath <nsoblath@gmail.com>
2025-05-02 11:18:12 -0700

    Added source file for iq_time_data.

--- source/data/iq_time_data.cc
+++ source/data/iq_time_data.cc
@@ -7,24 +7,80 @@
 
 #include "iq_time_data.hh"
 
+#include <cstring>
+
 namespace psyllid
 {
 
-    iq_time_data::iq_time_data( bool has_data ) :
-            f_has_data( has_data ),
-            f_array( ),
-            f_array_ptr( ), 
-            f_array_size( PAYLOAD_SIZE / 2 ),
-            f_pkt_in_session( 0 )
+    iq_time_data::iq_time_data() :
+            f_pkt_in_session( 0 ),
+            f_owns_data( false ),
+            f_array( nullptr ),
+            f_array_size( 0 )
+    {}
+
+    void iq_time_data::initialize( size_t a_size, iq_time_data::iq_t* a_external_array = nullptr )
     {
-        if( has_data )
+        // first take care of existing array, if there is any
+        if( f_owns_data )
         {
-            iq_time_data::set_array_ptr( f_array );
+            delete [] f_array;
         }
+
+        // then setup the new array
+        f_array_size = a_size;
+        f_owns_data = a_external_array == nullptr;
+        if( f_owns_data )
+        {
+            f_array = new iq_t[f_array_size];
+        }
+        else
+        {
+            f_array = a_external_array;
+        }
+
+        return;
+    }
+
+    iq_time_data& iq_time_data::operator=( const iq_time_data& a_orig )
+    {
+        if( f_owns_data && a_orig.f_owns_data && f_array_size != a_orig.f_array_size )
+        {
+            // shortcut case: both objects own the data and the array size matches
+            std::memcpy( f_array, a_orig.f_array, f_array_size * sizeof(iq_t) );
+        }
+        else
+        {
+            // generic case: dynamically delete and allocate arrays as needed based on object ownership
+            // if a_orig doesn't own the data, then just copy over the pointer
+            if( f_owns_data )
+            {
+                delete [] f_array;
+            }
+            if( a_orig.f_owns_data )
+            {
+                f_array = new iq_t[a_orig.f_array_size];
+                std::memcpy( f_array, a_orig.f_array, a_orig.f_array_size * sizeof(iq_t) );
+            }
+            else
+            {
+                f_array = a_orig.f_array;
+            }
+        }
+
+        f_pkt_in_session = a_orig.f_pkt_in_session;
+        f_owns_data = a_orig.f_owns_data;
+        f_array_size = a_orig.f_array_size;
+
+        return *this;
     }
 
     iq_time_data::~iq_time_data()
     {
+        if( f_owns_data )
+        {
+            delete [] f_array;
+        }
     }
 
 } /* namespace psyllid */
